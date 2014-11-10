fe6df1d159857328b51912bcfb3b4351f9d1435b
John Uba <johnuba17@gmail.com>
2014-11-10 12:59:28 -0800

    Test commit

---
+++ src/test/java/ServerTest.java
@@ -0,0 +1,20 @@
+package server;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+import org.junit.Test;
+
+import com.kickass.Server;
+
+public class ServerTest {
+
+	@Test
+	public void connectToPort5000() throws Exception {
+		Server s = new Server(new ServerSocket(5000));
+		assertEquals(5000, s.getPort());		
+	}
+
+}
