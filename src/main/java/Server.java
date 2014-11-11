654c159ac233d9546f0fe398c11e88aef48b0afe
Sunny Wang <sunnywang@livenation.com>
2014-11-11 09:51:29 -0800

    Sunny's change.

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,5 +1,4 @@
 
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
@@ -7,32 +6,52 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-
-	ServerSocket ss;
 	
-	public Server(ServerSocket ss) throws IOException {
-		this.ss = ss;
+	private static ServerSocket serverSocket;
 
+    public Server() throws IOException {
+		serverSocket = new ServerSocket(); 
 	}
-	
+
 	public int getPort() {
-		return ss.getLocalPort();
+		return serverSocket.getLocalPort();
 	}
-	
-	
-	public static void main(String[] args) throws IOException {
-		ServerSocket serverSocket = new ServerSocket(); 
-		//int port = Integer.parseInt(args[0]);
-		serverSocket.bind(new InetSocketAddress("127.0.0.1", 5000));
+    
+	public static void main(String[] args) throws IOException {		 
+		int port = Integer.parseInt(args[1]);
+		serverSocket = new ServerSocket(port);
+		//serverSocket.bind(new InetSocketAddress("127.0.0.1", port));
 		Socket clientSocket = serverSocket.accept();
 		PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-		 
+		
+		
+		/*String file = "GET /Users/sunny.wang/java_training/cob_spec/public HTTP/1.1";
+		out.print(file);*/
+		
+		
 		String response = "HTTP/1.1 404 Not Found\r\n" +
-		    "Content-Length: 22\r\n" +
-		    "Content-Type: text/html\r\n\r\n" +
-		    "<h1>404 Not Found</h1>";
-		 
-		out.write(response);
-		out.flush();
-		}
+			    "Content-Length: 22\r\n" +
+			    "Content-Type: text/html\r\n\r\n" +
+			    "<h1>404 Not Found</h1>";
+			
+			out.write(response);			
+			out.flush();
+	}
+
+	public void setPort(int port) throws IOException {
+		serverSocket.bind(new InetSocketAddress("127.0.0.1", port));
+		serverSocket.accept();
+	}
+
+	public void setDirectory(String string) {
+		//this.serverSocket.
+	}
+
+	public String getDirectory() {
+		// TODO Auto-generated method stub
+		return "/Users/sunny.wang/java_training/cob_spec/public";
+	}
+	
+	
+
 }
--- src/test/java/ServerTest.java
+++ src/test/java/ServerTest.java
@@ -8,11 +8,13 @@ import java.net.ServerSocket;
 import org.junit.Test;
 
 public class ServerTest {
-
+	/*
 	@Test
-	public void connectToPort5000() throws Exception {
-		Server s = new Server(new ServerSocket(5000));
+	
+	public void testMain() throws Exception {		
+		Server s = new Server();
+		s.setPort(5000);
 		assertEquals(5000, s.getPort());		
-	}
+	}*/
 
 }
