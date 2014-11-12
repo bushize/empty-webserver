df40323ed9ca7af4aa41c514c8c377a98228325b
Sunny Wang <sunnywang@livenation.com>
2014-11-11 19:16:51 -0800

    Created HttpResponse and httpResponseTest.

--- src/main/java/ProcessThread.java
+++ src/main/java/ProcessThread.java
@@ -3,6 +3,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.StringTokenizer;
 
@@ -10,23 +11,27 @@ import java.util.StringTokenizer;
 public class ProcessThread extends Thread {
 	private Socket socket;
 	private String response;
-	private String path;
+	private String baseurl;
+	private ServerSocket serverSocket;
 	
 	public ProcessThread(Socket socket, String path){
-		this.socket = socket;
-		this.path = path;
+		//this.socket = socket;
+		//this.path = path;
 	}
 	
+	public ProcessThread(int port, String path) throws IOException {
+		serverSocket = new ServerSocket(port);
+		socket = serverSocket.accept();
+		this.baseurl = path;
+	}
+
 	public void run() {
 						
 		PrintWriter out;
 		BufferedReader in;
 		
-		try {
-					
-			in = new BufferedReader(
-			        new InputStreamReader(socket.getInputStream()));
-			
+		try {					
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));			
 			StringTokenizer tokenizedLine = new StringTokenizer(in.readLine());
 			String httpMethod = tokenizedLine.nextToken();
 			String path = tokenizedLine.nextToken();
@@ -34,32 +39,7 @@ public class ProcessThread extends Thread {
 			System.out.println("RECV:" + httpMethod);
 			System.out.println("RECV:" + path);
 			
-			String response200 = "HTTP/1.1 200 OK\r\n" +
-				    "Content-Length: 0\r\n" +
-				    "Content-Type: text/html\r\n\r\n" +
-				    "<h1>200 OK</h1>";
-			
-			String response404 = "HTTP/1.1 404 Not Found\r\n" +
-				    "Content-Length: 0\r\n" +
-				    "Content-Type: text/html\r\n\r\n" +
-				    "<h1>404 Not Found</h1>";
-			
-			String response400 = "HTTP/1.1 400 Not Found\r\n" +
-				    "Content-Length: 0\r\n" +
-				    "Content-Type: text/html\r\n\r\n" +
-				    "<h1>400 bad request</h1>";
-			
-			if (httpMethod.equals("GET")) {
-				if(pathIsGood(path)) {
-					response = response200;	
-				} else {
-					response = response404;
-				}
-			} else if (httpMethod.equals("POST") || httpMethod.equals("PUT")) {
-				response = response200;
-			} else {
-				 response = response400;
-			}
+			response = getResponse(httpMethod, path);
 			
 			out = new PrintWriter(socket.getOutputStream(), true);
 			out.write(response);
@@ -72,13 +52,53 @@ public class ProcessThread extends Thread {
 			
 	}
 	
-	public String getResponse() {		
+	public String getResponse(String httpMethod, String path) {
+		String response200 = "HTTP/1.1 200 OK\r\n" +
+			    "Content-Length: 0\r\n" +
+			    "Content-Type: text/html\r\n\r\n" +
+			    "<h1>200 OK</h1>";
+		
+		String response404 = "HTTP/1.1 404 Not Found\r\n" +
+			    "Content-Length: 0\r\n" +
+			    "Content-Type: text/html\r\n\r\n" +
+			    "<h1>404 Not Found</h1>";
+		
+		String response400 = "HTTP/1.1 400 Not Found\r\n" +
+			    "Content-Length: 0\r\n" +
+			    "Content-Type: text/html\r\n\r\n" +
+			    "<h1>400 bad request</h1>";
+		String responseOptions = "HTTP/1.1 200 OK\r\n" +
+			    "Content-Length: 0\r\n" +
+			    "Allow: GET,HEAD,POST,OPTIONS,PUT\n" +
+			    "Content-Type: text/html\r\n\r\n" +
+			    "<h1>200 OK</h1>";
+		
+		if (httpMethod.equals("GET")) {
+			if(pathIsGood(path)) {
+				response = response200;	
+			} else {
+				response = response404;
+			}
+		} else if (httpMethod.equals("POST") || httpMethod.equals("PUT") 
+				|| httpMethod.equals("DELETE")) {
+			response = response200;
+		} else if(httpMethod.equals("OPTIONS")) {			
+			response = responseOptions;
+		} else {
+			response = response400;
+		}
 		return response;
 	}
 	
 	private boolean pathIsGood(String p) {
 		
-		File f = new File(path + p);
+		String fileName = p;
+        if (fileName.startsWith("/"))
+           fileName = baseurl + fileName;
+        else
+           fileName = baseurl + "/" + fileName;
+        
+		File f = new File(fileName);
 		
 		if (!f.exists())
 			return false;
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -8,6 +8,7 @@ import java.net.Socket;
 
 public class Server {
 	public static void main(String[] args) throws IOException {
+		/*
 		ServerSocket ss = new ServerSocket(Integer.parseInt(args[1]));
 		String path = args[3];
 		try {
@@ -18,5 +19,12 @@ public class Server {
 		finally {
 			ss.close();
 		}
+		*/
+		int port = Integer.parseInt(args[1]);
+		String path = args[3];
+		
+        Thread process = new ProcessThread(port, path);
+        process.start();
+		
 	}
 }
