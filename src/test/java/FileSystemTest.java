d0f3e857d7a18d9658770389156e060c129a97e4
Sunny Wang <sunnywang@livenation.com>
2014-11-12 23:29:36 -0800

    Renamed directory to filesystem.

---
+++ src/test/java/FileSystemTest.java
@@ -0,0 +1,39 @@
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class FileSystemTest {
+	FileSystem dir;
+	String file = "/Users/sunny.wang/java_training/cob_spec/public/test";
+	
+	@Before
+	public void SetUp() {
+		 dir = new FileSystem();
+	}
+	
+	@Test
+	public void rootShouldExist() throws Exception {		
+		assertEquals(true, dir.isExisting(file));
+	}
+	
+	@Test
+	public void rootShouldBeADirectory() throws Exception {
+		assertEquals(true, dir.isDirectory(file));
+	}
+
+	@Test
+	public void returnFilesList() throws Exception {		
+		File d = new File(file);
+		assertEquals(d.list().length, dir.getFiles(file).length);
+	}
+	
+	@Test
+	public void shouldNotReturnHiddenFile() throws Exception {
+		
+	}
+
+}
