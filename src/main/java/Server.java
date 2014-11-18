434178b321faa5b7c2d4db4476293ab5ab0a4b06
John Uba <johnuba17@gmail.com>
2014-11-18 11:23:37 -0800

    test in progress

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -3,29 +3,44 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
+	
+	private String directory;
+	private int port;
+	private ServerSocket ss;
+	
+	public Server(int port, String directory) throws IOException {
+		this.port = port;
+		this.directory = directory;
+		this.ss = new ServerSocket(port);
+	}
+	
+	public void begin() throws IOException {
+		try {			
+			Socket socket = ss.accept();
+	        Thread process = new Process(socket, directory);
+	        process.start();	
+	        
+	        ss.close();
+		}
+		catch (Exception e) {			
+			System.out.println("Error: " + e.getMessage());
+			e.printStackTrace();
+	
+		}		
+	}
+	
+	public String getServerDirectory() {
+		return directory;
+	}
+	
+	public int getPort() {
+		return port;
+	}
+	
 	public static void main(String[] args) throws IOException {
-		
-		ServerSocket ss;
-		String path;
-		
-		while(true) {
-		
-			try {
-				ss = new ServerSocket(Integer.parseInt(args[1]));
-				path = args[3];
-				
-				Socket socket = ss.accept();
-		        Thread process = new Process(socket, path);
-		        process.start();
-		        
-		        ss.close();
-			}
-			catch (Exception e) {			
-				System.out.println("Error: " + e.getMessage());
-				e.printStackTrace();
-				break;
-			}
-		
+		while (true) {
+			Server s = new Server(Integer.parseInt(args[1]), args[3]);		
+			s.begin();		
 		}
 	}
 }
